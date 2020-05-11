8c2b58bb6f234d59481d53294cd60d75b72ac1a8
Reshab Gupta <resgupta@amazon.com>
2020-05-11 21:31:32 +0530

    Added print primes funtion

--- lesson-2-containers/exercises/simple-node/server.js
+++ lesson-2-containers/exercises/simple-node/server.js
@@ -2,10 +2,21 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function printPrime(n) {
+  for (let i = 2; i * i <= n; ++i) {
+    if (n % i == 0) {
+      return;
+    }
+  }
+  console.log(n);
+}
+
 async function main() {
-  while(true) {
+  let no = 2;
+  while (true) {
     console.log('Containers rule!');
     await sleep(5000);
+    printPrime(no++);
   }
 }
 
